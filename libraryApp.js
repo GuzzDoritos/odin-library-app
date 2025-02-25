1333f14149ced90a0c5bbed778189b101c5620a4
Guzz <gustavo.bragada@hotmail.com>
2025-02-25 17:52:55 -0300

    Create array and constructor for book objects

--- libraryApp.js
+++ libraryApp.js
@@ -1,3 +1,12 @@
+const bookList = [];
+
+function Book(author, title, genre, pages, read) {
+  this.author = author;
+  this.title = title;
+  this.genre = genre;
+  this.pages = pages;
+  this.read = read;
+}
 const addBookButton = document.querySelector("#add-book");
 const addBookModal = document.querySelector("dialog");
 const modalCloseButton = document.querySelector("#modal-close-button");
