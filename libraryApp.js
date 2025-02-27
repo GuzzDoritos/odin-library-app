ce0895b32131e784b8003781a8582f7b9ac583e9
Guzz <gustavo.bragada@hotmail.com>
2025-02-27 13:33:47 -0300

    Refactor code

--- libraryApp.js
+++ libraryApp.js
@@ -1,130 +1,143 @@
-const bookList = [];
+const libraryApp = (function () {
+  //array for storing registered books
+  const bookList = [];
 
-function Book(author, title, genre, pages, read) {
-  this.author = author;
-  this.title = title;
-  this.genre = genre;
-  this.pages = pages;
-  this.read = read;
-}
-
-Book.prototype.toggleRead = function () {
-  this.read = this.read ? false : true;
-  updateList();
-};
-
-function addBooktoList() {
-  let bookArgs = [];
-  for (prop in bookForm) {
-    if (bookForm[prop].value.trim() == "") {
-      console.log("nope");
-      return;
-    }
-    bookArgs.push(bookForm[prop].value);
+  //book constructor
+  function Book(author, title, genre, pages, read) {
+    this.author = author;
+    this.title = title;
+    this.genre = genre;
+    this.pages = pages;
+    this.read = read;
   }
-  let book = new Book(...bookArgs);
-  book.read = book.read === "true";
-  bookList.push(book);
 
-  finishInput();
-}
+  Book.prototype.toggleRead = function () {
+    this.read = this.read ? false : true;
+    updateList();
+  };
 
-function finishInput() {
-  updateList();
-  clearInput();
-  addBookModal.close();
-}
+  // cacheDom
+  const bookForm = {
+    authorInput: document.querySelector("#book-author"),
+    bookNameInput: document.querySelector("#book-name"),
+    genreInput: document.querySelector("#book-genre"),
+    pagesInput: document.querySelector("#book-pages"),
+    hasRead: document.querySelector("#is-book-finished"),
+  };
 
-function updateList() {
+  const addBookButton = document.querySelector("#add-book");
+  const addBookModal = document.querySelector("dialog");
+  const modalCloseButton = document.querySelector("#modal-close-button");
+  const submitBookBtn = document.querySelector("#submit-book");
   const booksTable = document.querySelector("#books-table");
 
-  booksTable.textContent = "";
+  // bind events
 
-  for (book of bookList) {
-    const bookRow = document.createElement("tr");
+  submitBookBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    addBooktoList();
+  });
 
-    bookRow.dataset.id = bookList.indexOf(book);
+  addBookButton.addEventListener("click", () => {
+    addBookModal.showModal();
+  });
 
-    for (prop in book) {
-      if (Object.hasOwn(book, prop)) {
-        const bookCell = document.createElement("td");
-        if (prop === "read") {
-          const toggleButton = createToggleButton(book);
-          toggleButton.addEventListener("click", () => {
-            bookList[toggleButton.parentElement.parentElement.dataset.id].toggleRead();
-            updateList();
-          });
+  modalCloseButton.addEventListener("click", () => {
+    addBookModal.close();
+  });
 
-          const toggleButtonCell = document.createElement("td");
-          toggleButtonCell.appendChild(toggleButton);
-          bookRow.appendChild(toggleButtonCell);
-          continue;
+  // render function
+
+  function updateList() {
+    booksTable.textContent = "";
+
+    for (book of bookList) {
+      const bookRow = document.createElement("tr");
+
+      bookRow.dataset.id = bookList.indexOf(book);
+
+      for (prop in book) {
+        if (Object.hasOwn(book, prop)) {
+          const bookCell = document.createElement("td");
+          if (prop === "read") {
+            const toggleButton = createToggleButton(book);
+            toggleButton.addEventListener("click", () => {
+              bookList[bookRow.dataset.id].toggleRead();
+              updateList();
+            });
+
+            bookCell.appendChild(toggleButton);
+            bookRow.appendChild(bookCell);
+            continue;
+          }
+          bookCell.textContent = book[prop];
+          bookRow.appendChild(bookCell);
         }
-        bookCell.textContent = book[prop];
-        bookRow.appendChild(bookCell);
       }
+
+      const deleteBtn = createDeleteButton();
+      deleteBtn.addEventListener("click", () => {
+        deleteBook(bookRow.dataset.id);
+      });
+
+      const deleteCell = document.createElement("td");
+      deleteCell.className = "delete-cell";
+
+      deleteCell.appendChild(deleteBtn);
+      bookRow.appendChild(deleteCell);
+
+      booksTable.appendChild(bookRow);
     }
-
-    const deleteBtn = createDeleteButton();
-    deleteBtn.addEventListener("click", () => {
-        deleteBook(deleteBtn);
-    })
-
-    const deleteCell = document.createElement("td");
-    deleteCell.className = "delete-cell";
-
-    deleteCell.appendChild(deleteBtn);
-    bookRow.appendChild(deleteCell);
-
-    booksTable.appendChild(bookRow);
   }
-}
 
-function deleteBook(delBtn) {
-    bookList.splice(delBtn.parentElement.parentElement.dataset.id, 1);
+  // auxiliary functions for creating buttons in render
+
+  function createDeleteButton() {
+    const deleteBtn = document.createElement("button");
+    deleteBtn.className = "delete-btn";
+    deleteBtn.textContent = "Delete";
+    return deleteBtn;
+  }
+
+  function createToggleButton(bookObj) {
+    const btn = document.createElement("button");
+    btn.className = bookObj.read ? "has-been-read" : "has-not-been-read";
+    btn.textContent = bookObj.read ? "Yes" : "No";
+    return btn;
+  }
+
+  // Add and delete buttons functions
+
+  function addBooktoList() {
+    let bookArgs = [];
+    for (prop in bookForm) {
+      if (bookForm[prop].value.trim() == "") {
+        console.log("nope");
+        return;
+      }
+      bookArgs.push(bookForm[prop].value);
+    }
+    let book = new Book(...bookArgs);
+    book.read = book.read === "true";
+    bookList.push(book);
+
+    finishInput();
+  }
+
+  function finishInput() {
     updateList();
-}
+    clearInput();
+    addBookModal.close();
+  }
 
-function clearInput() {
-  for (prop in bookForm) bookForm[prop].value = "";
-}
+  function deleteBook(index) {
+    bookList.splice(index, 1);
+    updateList();
+  }
 
-function createDeleteButton() {
-  const deleteBtn = document.createElement("button");
-  deleteBtn.className = "delete-btn";
-  deleteBtn.textContent = "Delete";
-  return deleteBtn;
-}
+  // clear form inputs
 
-function createToggleButton(bookObj) {
-  const btn = document.createElement("button");
-  btn.className = bookObj.read ? "has-been-read" : "has-not-been-read";
-  btn.textContent = bookObj.read ? "Yes" : "No";
-  return btn;
-}
-
-const bookForm = {
-  authorInput: document.querySelector("#book-author"),
-  bookNameInput: document.querySelector("#book-name"),
-  genreInput: document.querySelector("#book-genre"),
-  pagesInput: document.querySelector("#book-pages"),
-  hasRead: document.querySelector("#is-book-finished"),
-};
-
-const addBookButton = document.querySelector("#add-book");
-const addBookModal = document.querySelector("dialog");
-const modalCloseButton = document.querySelector("#modal-close-button");
-const submitBookBtn = document.querySelector("#submit-book");
-
-submitBookBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  addBooktoList();
-});
-
-addBookButton.addEventListener("click", () => {
-  addBookModal.showModal();
-});
-
-modalCloseButton.addEventListener("click", () => {
-  addBookModal.close();
-});
+  function clearInput() {
+    for (prop in bookForm) bookForm[prop].value = "";
+  }
+})();
